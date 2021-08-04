4a275c63a5a3aa32351b8ebd26de527a0cc7cbe4
Its1010 <87544398+its1010dev@users.noreply.github.com>
2021-08-04 10:07:15 +0300

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,14 @@
+import string
+import random
+import os
+x=100
+
+
+
+while not (x == 0) :
+            
+  tokens = (''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(16)))
+  x -= 1
+  a_file = open("k3ny_nitro.txt", "a" )
+  a_file.write('discord.gift/'+tokens + '\n')
+  a_file.close()
